d8b68c890b4f422b76b42282e087d088212aeee3
monster1935 <monster.1935.li@gmail.com>
2024-03-05 17:29:26 +0800

    feat: style change

--- components/FeeType.tsx
+++ components/FeeType.tsx
@@ -7,7 +7,7 @@ interface IProps {
 }
 
 export default function FeeType({ type, amount, focus, onClick }: IProps) {
-    return <div className={`flex flex-col items-center border rounded-[12px] border-[#e7e7e1] px-6 py-2 cursor-pointer w-[218px] h-[79px] ${focus ? 'border-[#000]' : 'border-[#e7e7e1]'} `} onClick={onClick}>
+    return <div className={`flex flex-col items-center border rounded-[12px] border-[#e7e7e1] px-6 py-2 cursor-pointer w-[218px] h-[79px] ${focus ? 'border-black' : 'border-[#e7e7e1]'} `} onClick={onClick}>
         <div className="mb-2 font-bold">{type}</div>
         <div>{amount} sats/vB</div>
     </div>
