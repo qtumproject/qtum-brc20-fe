6fb069ec7722962dee364a64d70b648eb4772631
monster1935 <monster.1935.li@gmail.com>
2024-05-20 09:27:43 +0800

    feat: optimize small screen UI

---
+++ theme/components/Drawer.ts
@@ -0,0 +1,26 @@
+import { modalAnatomy as parts } from '@chakra-ui/anatomy'
+import { createMultiStyleConfigHelpers } from '@chakra-ui/styled-system'
+
+const { definePartsStyle, defineMultiStyleConfig } =
+    createMultiStyleConfigHelpers(parts.keys)
+
+const baseStyle = definePartsStyle({
+    // define the part you're going to style
+    overlay: {
+        bg: 'rgba(247,247,240,0.84)', //change the background
+        _dark: {
+            bg: 'rgba(34, 37, 45, 0.97)'
+        }
+    },
+    dialog: {
+        borderTopLeftRadius: '24px',
+        borderTopRightRadius: '24px',
+    },
+    body: {
+        minHeight: '300px',
+    }
+})
+
+export const drawerTheme = defineMultiStyleConfig({
+    baseStyle,
+})
